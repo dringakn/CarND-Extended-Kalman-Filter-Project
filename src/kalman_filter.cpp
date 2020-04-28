d8fdae291e2b34110a6e53aa6d37416d93a806cf
AKN <dringakn@gmil.com>
2020-04-29 00:44:56 +0100

    feat: Update Kalman filter Update step
    Implement state vector and covariance matrixe update equations.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -23,14 +23,20 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  x_ = F_ * x_; // State update
+  x_ = F_ * x_;                       // State update
   P_ = F_ * P_ * F_.transpose() + Q_; // State covariance update
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+  VectorXd y = z - H_ * x_; // residue
+  MatrixXd Ht = H_.transpose(); // intermediate
+  MatrixXd S = H_ * P_ * Ht + R_; // innovation
+  MatrixXd Si = S.inverse(); // innovation inverse
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si; // Kalman gain
+  x_ = x_ + (K * y); // update states
+  MatrixXd I = MatrixXd::Identity(x_.size(), x_.size());
+  P_ = (I - K * H_) * P_; // update state covariance
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
